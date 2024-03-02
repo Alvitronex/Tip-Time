c2adfca352306bfc457d936955379b691712cd5d
Alvitronex <marioenriquealvarado12@gmail.com>
2024-03-01 21:23:43 -0600

    Init project

---
+++ app/src/main/java/com/example/salarycalculator/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.salarycalculator
+
+import android.icu.number.NumberFormatter
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.salarycalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+//        binding.calculateButton.isEnabled = false
+//        binding.costOfService.addTextChangedListener {
+//            binding.calculateButton.isEnabled = it.toString().isNotEmpty()
+//        }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = getString(R.string.tip_amount, NumberFormat.getCurrencyInstance().format(0.00))
+            //binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
